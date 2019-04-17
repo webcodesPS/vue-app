512616e209a9d43f79c53651f5966a994ede24cd
pskaczukowski <przemyslaw.skaczukowski@upaid.pl>
2019-04-17 19:44:59 +0200

    add

---
+++ src/components/ToDos.vue
@@ -0,0 +1,31 @@
+<template>
+  <h1>
+
+    <div v-for="todo in todos">{{ todo.text }}</div>
+
+    <button v-on:click="add">add</button>
+  </h1>
+</template>
+
+<script lang="ts">
+import { Vue } from "vue-property-decorator";
+
+export default class ToDos extends Vue {
+
+  public todos: any = [
+    {
+      text: "Do the dishes"
+    },
+    {
+      text: "Take out the trash"
+    },
+    {
+      text: "Mow the lawn"
+    }
+  ];
+
+  add() {
+      console.log('aaaaaa');
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <ToDos />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import ToDos from "@/components/ToDos.vue"; // @ is an alias to /src
 
 @Component({
   components: {
-    HelloWorld
+      ToDos
   }
 })
 export default class Home extends Vue {}
